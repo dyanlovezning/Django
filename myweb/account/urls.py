2d6a3f1bbc0fa76fe4edadc440002324f7b11070
Dyan <260680524@qq.com>
2019-09-03 18:41:31 +0800

    添加注册，登陆

--- myweb/account/urls.py
+++ myweb/account/urls.py
@@ -1,8 +1,14 @@
 from django.conf.urls import url
+from django.contrib.auth.views import LoginView, LogoutView
 from . import views
 from django.conf import settings
 app_name='account'
 
 urlpatterns = [
-    url(r'^login/$', views.user_login, name="user_login"),
+    #url(r'^login/$', views.user_login, name="user_login"),
+    url(r'^login/$', LoginView.as_view(template_name='registration/login.html'), name="user_login"),
+    url(r'^newlogin/$', LoginView.as_view(template_name='account/login.html'), name="user_login"),
+    #url(r'^logout/$', LogoutView.as_view(), name="user_logout"),
+    url(r'^logout/$', LogoutView.as_view(template_name='account/logout.html'), name="user_logout"),
+    url(r'^register/$', views.register, name="user_register"),
 ]
